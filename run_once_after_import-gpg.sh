a3b45aa1f99be43229d4aba95f0233563f6379d2
Islam Mansour <me@imansour.net>
2022-10-30 08:40:10 +0100

    Update .bashrc_linux Update .config/chezmoi/chezmoi.toml Add .gnupg/gpg.conf Add import-gpg.sh Add install-packages.sh

---
+++ run_once_after_import-gpg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# gpg --keyserver hkps://keys.openpgp.org --recv-keys 712B106A06F5ACEC
+
+# $ gpg --import <key.asc
+# $ (echo 5; echo y; echo save) |
+#   gpg --command-fd 0 --no-tty --no-greeting -q --edit-key "$(
+#   curl https://keybase.io/imansour/pgp_keys.asc | gpg --list-packets |
+#   awk '$1=="keyid:"{print$2;exit}')" trust 
+
+### Import public keys from keybase
+curl https://keybase.io/imansour/pgp_keys.asc | gpg --import
+
+### Trust all import keys from keybase
+curl https://keybase.io/imansour/pgp_keys.asc | gpg --list-packets | grep -oP '(?<=keyid )(\w+)' | while read -r line ; do
+    echo "Trusting $line"
+    # your code goes here
+    (echo 5; echo y; echo save) |
+  gpg --command-fd 0 --no-tty --no-greeting -q --edit-key "$line" trust 
+done
